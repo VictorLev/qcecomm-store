f485bde3c13b4b1567969e6c70bbca59e1359e61
Victor <levesque.vic@gmail.com>
2023-10-28 17:47:11 +1100

    get product

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -3,8 +3,23 @@ import qs from "query-string"
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
 
-const getProducts = async ():Promise<Product[]> => {
-    //const url = qs.stringifyUrl({   })
+interface Query {
+    categoryId?: string;
+    colorId?: string;
+    sizeId?: string;
+    isFeatured?: boolean;
+}
+
+const getProducts = async ( query:Query ): Promise<Product[]> => {
+    const url = qs.stringifyUrl({ 
+        url: URL,
+        query: {
+            colorId: query.colorId,
+            sizeId: query.sizeId,
+            categoryId: query.categoryId,
+            isFeatured: query.isFeatured
+        }
+      })
     
     const res = await fetch(URL);
     return res.json();
