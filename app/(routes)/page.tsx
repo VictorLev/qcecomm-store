f28d0c273e8520359337970c7737a02e80a764de
Victor <levesque.vic@gmail.com>
2023-10-28 22:53:17 +1100

    commit

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,4 @@
 import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard"
 import Container from "@/components/ui/container"
 
@@ -7,9 +6,6 @@ export const revalidate = 0;
 
 const HomePage = async () => {
     const billboard = await getBillboard("8e82fde3-4f18-4c8c-8e9e-878a7674e026");
-    const products = await getProducts({isFeatured: true});
-
-
     return(
         <Container>
             <div className="space-y-10 pb-10">
