7f925d10708cac4c696be91d921f5ad7f2030516
Victor <levesque.vic@gmail.com>
2023-10-28 23:24:40 +1100

    update

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,23 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard"
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container"
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true})
     const billboard = await getBillboard("8e82fde3-4f18-4c8c-8e9e-878a7674e026");
+    
+    
     return(
         <Container>
             <div className="space-y-10 pb-10">
-                <Billboard data={billboard} /> 
+                <Billboard data={billboard} />
             </div>
             <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                
+                <ProductList title="Featured Products" items={products}/>
             </div>
         </Container>
     )
---
+++ components/product-list.tsx
@@ -0,0 +1,20 @@
+import { Product } from "@/type";
+
+interface ProductListProps {
+    title:string;
+    items: Product[];
+}
+
+const ProductList: React.FC<ProductListProps> = ({
+    title,
+    items
+}) => {
+    return (
+        <div className="space-y-4">
+            <h3>{title}</h3>
+        </div>
+    );
+    
+}
+ 
+export default ProductList;
