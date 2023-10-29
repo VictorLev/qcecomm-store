f73b093520cb3f160c21371204e350aad0c4e62c
Victor <levesque.vic@gmail.com>
2023-10-29 16:17:20 +1100

    product single page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
     data: Product;
@@ -13,8 +14,15 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const router = useRouter()
+
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`)
+    }
+
+
     return ( 
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Images and Actions */}
             <div className="aspect-square rounded-xl bg-blue-100 relative">
                 <Image 
