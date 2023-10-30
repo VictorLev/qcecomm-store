1e42a640f85772b31a53b6a7c086bf1596fae1de
Victor <levesque.vic@gmail.com>
2023-10-30 13:20:50 +1100

    commit Product info page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,7 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
@@ -22,9 +24,9 @@ const ProductPage: React.FC<ProductPageProps> = async ({
         <Container>
             <div className="px-4 py-10 sm:px-6 lg:px-8">
                 <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                    <div>Gallery</div>
+                    <Gallery images={product.images}/>
                     <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                        <div>Info</div>
+                        <Info data={product}/>
                     </div>
                 </div>
 
--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -1,4 +1,4 @@
-"use Client"
+"use client"
 
 import { useEffect, useState } from "react";
 
