b3ad360f861563a45dea30e0bd5bc23b73f246e3
Victor <levesque.vic@gmail.com>
2023-10-31 18:45:14 +1100

    mobile filter

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,9 @@ import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -40,6 +43,8 @@ const CategoryPage: React.FC<CategoryPageProps> =  async ({
                 />
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        <MobileFilters sizes={sizes} colors={colors} />
+                        
                         <div className="hidden lg:block">
                             <Filter 
                                 valueKey="sizeId"
@@ -51,6 +56,14 @@ const CategoryPage: React.FC<CategoryPageProps> =  async ({
                                 name="Colors"
                                 data={colors}
                             />
+                        </div>
+                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                            {products.length === 0 && <NoResults />}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {products.map((item) => (
+                                    <ProductCard key={item.id} data={item}/>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
